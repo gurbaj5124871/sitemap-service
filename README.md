1365c8a393de402175fdc909f4a7fe9ab88ae54a
Gurbaj Singh <singh.gurbaj5124871@gmail.com>
2022-08-20 18:16:34 +0530

    enhance: prisma db models for sitemaps written, readme.md updated

--- README.md
+++ README.md
@@ -2,24 +2,44 @@
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo_text.svg" width="320" alt="Nest Logo" /></a>
 </p>
 
-## Description
+# Description
 
 Automated sitemap generator pipeline which automatically creates or amends sitemap files with new links and register them with popular crawlers like google.
+<br/>
+<br/>
 
 ## Technologies
 
-1. [Apache Kafka](https://kafka.apache.org/) : For real time updates of newly created links with relevant data
-2. [PostgreSQL](https://www.postgresql.org/) : For persistance, locking and mapping with sitemap files
+1. [Apache Kafka](https://kafka.apache.org/): For real time updates of newly created links with relevant data
+2. [PostgreSQL](https://www.postgresql.org/): For persistance, locking and mapping with sitemap files
+3. [AWS S3](https://aws.amazon.com/s3/): For persisting actual sitemap xml files
 
 ## Dependencies
 
 1. [NestJS](https://docs.nestjs.com/) : A progressive Node.js framework for building efficient and scalable server-side applications.
 2. [Prisma.io](https://www.prisma.io/docs/getting-started) : Next-generation Node.js and TypeScript ORM
+3. [kafkaJS](https://kafka.js.org/docs/getting-started) : Kafka client for nodeJS
 
-## Installation
+<br/>
+
+# Getting Started
+
+Create `.env` and override custom environment values
+
+```bash
+$ cat .env.example > .env
+```
+
+Install dependencies
 
 ```bash
 $ npm install
+```
+
+Run DB Migrations
+
+```bash
+$ npx prisma migrate dev
 ```
 
 ## Running the app
@@ -35,6 +55,19 @@ $ npm run start:dev
 $ npm run start:prod
 ```
 
+## Running the app via docker compose
+
+The compose file provides:
+
+1. Apache Kafka with Zookeeper and will automatically creating necessary topics
+2. UI for kafka and will be accessible at port 8080
+3. PostgreSQL 13 running on port 5432
+4. UI for postgreSQL named Admirer which will be accessible at port 8081
+
+```bash
+docker compose -f docker-compose.yaml up -d
+```
+
 ## Test
 
 ```bash
@@ -47,6 +80,9 @@ $ npm run test:e2e
 # test coverage
 $ npm run test:cov
 ```
+
+<br/>
+<br/>
 
 ## License
 
---
+++ prisma/migrations/20220820121633_create_tables/migration.sql
@@ -0,0 +1,119 @@
+-- CreateEnum
+CREATE TYPE "SitemapDeletionCycle" AS ENUM ('NOT_DELETED', 'MARKED_FOR_DELETION', 'DELETED');
+
+-- CreateEnum
+CREATE TYPE "SitemapFileLifeCycle" AS ENUM ('UPLOADED', 'LINKED_TO_INDEX', 'MARKED_FOR_INDEX_LASTMOD_UPDATE');
+
+-- CreateEnum
+CREATE TYPE "SitemapFileEntityType" AS ENUM ('TEXT', 'VIDEO');
+
+-- CreateEnum
+CREATE TYPE "SitemapIndexFileLifeCycle" AS ENUM ('UPLOADED', 'LINKED_TO_ROBOTS_DOT_TXT');
+
+-- CreateTable
+CREATE TABLE "TextSitemapModulusCounter" (
+    "id" INTEGER NOT NULL,
+    "counter" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "TextSitemapModulusCounter_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TextSitemap" (
+    "id" BIGINT NOT NULL,
+    "link" TEXT NOT NULL,
+    "modulusHashBase" INTEGER NOT NULL,
+    "modulusHashValue" INTEGER NOT NULL,
+    "counter" INTEGER NOT NULL,
+    "fileName" TEXT,
+    "isIgnored" BOOLEAN NOT NULL DEFAULT false,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "sitemapDeletionCycle" "SitemapDeletionCycle" NOT NULL DEFAULT 'NOT_DELETED',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "TextSitemap_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VideoSitemapModulusCounter" (
+    "id" INTEGER NOT NULL,
+    "counter" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "VideoSitemapModulusCounter_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SessionRecordingClip" (
+    "id" INTEGER NOT NULL,
+    "link" TEXT NOT NULL,
+    "videoURL" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "thumbnail" TEXT NOT NULL,
+    "startTimestamp" TIMESTAMP(3) NOT NULL,
+    "endTimestamp" TIMESTAMP(3) NOT NULL,
+    "actualDurationInSeconds" INTEGER NOT NULL,
+    "modulusHashBase" INTEGER NOT NULL,
+    "modulusHashValue" INTEGER NOT NULL,
+    "counter" INTEGER NOT NULL,
+    "fileName" TEXT,
+    "isIgnored" BOOLEAN NOT NULL DEFAULT false,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "sitemapDeletionCycle" "SitemapDeletionCycle" NOT NULL DEFAULT 'NOT_DELETED',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "SessionRecordingClip_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SitemapFile" (
+    "fileName" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+    "lifeCycle" "SitemapFileLifeCycle" NOT NULL DEFAULT 'UPLOADED',
+    "location" TEXT NOT NULL,
+    "entityType" "SitemapFileEntityType" NOT NULL,
+    "indexFileName" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "SitemapFile_pkey" PRIMARY KEY ("fileName")
+);
+
+-- CreateTable
+CREATE TABLE "SitemapIndexFile" (
+    "fileName" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+    "lifeCycle" "SitemapIndexFileLifeCycle" NOT NULL DEFAULT 'UPLOADED',
+    "location" TEXT NOT NULL,
+    "entityType" "SitemapFileEntityType" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "SitemapIndexFile_pkey" PRIMARY KEY ("fileName")
+);
+
+-- CreateTable
+CREATE TABLE "JobLock" (
+    "key" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "JobLock_pkey" PRIMARY KEY ("key")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "JobLock_key_key" ON "JobLock"("key");
+
+-- AddForeignKey
+ALTER TABLE "TextSitemap" ADD CONSTRAINT "TextSitemap_fileName_fkey" FOREIGN KEY ("fileName") REFERENCES "SitemapFile"("fileName") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SessionRecordingClip" ADD CONSTRAINT "SessionRecordingClip_fileName_fkey" FOREIGN KEY ("fileName") REFERENCES "SitemapFile"("fileName") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SitemapFile" ADD CONSTRAINT "SitemapFile_indexFileName_fkey" FOREIGN KEY ("indexFileName") REFERENCES "SitemapIndexFile"("fileName") ON DELETE RESTRICT ON UPDATE CASCADE;
